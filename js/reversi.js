de8c25b4284e9cc8a55f59fd2cb5332046c50f94
Alexander Berson <aberson@yahoo.com>
2021-10-18 10:38:29 +0300

    random ai

--- js/reversi.js
+++ js/reversi.js
@@ -3,8 +3,9 @@ let empty = 0;
 let black = 1;
 let white = 2;
 let gray = -1;
+let draw = false;
 
-let color = white;
+let color = black;
 
 const showBoard = () => document.querySelector("body").style.opacity = 1;
 
@@ -12,16 +13,27 @@ const resetBoard = () => board = Array.from(Array(numberOfRows), _ => Array(numb
 
 const touchScreen = () => matchMedia('(hover: none)').matches;
 
+const reverseColor = () => color = color == black ? white : black;
+
 const setBoard = () => {
 
     board = [[0,0,0,0,0,0,0,0],
-             [0,2,0,0,0,0,0,0],
-             [0,2,0,0,0,0,0,0],
-             [0,2,1,2,1,0,0,0],
-             [0,1,1,1,2,0,0,0],
-             [0,0,2,0,1,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0],
+             [0,0,0,2,1,0,0,0],
+             [0,0,0,1,2,0,0,0],
+             [0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0]];
+
+    // board = [[0,1,1,1,1,0,2,1],
+    //          [0,2,2,2,2,2,0,1],
+    //          [2,2,2,2,2,2,2,1],
+    //          [2,2,2,2,2,2,2,1],
+    //          [2,2,2,2,2,2,2,1],
+    //          [2,2,2,2,2,2,2,1],
+    //          [2,2,2,2,2,2,2,1],
+    //          [0,2,2,2,2,2,0,0]];
 }
 
 const redrawBoard = () => {
@@ -31,17 +43,14 @@ const redrawBoard = () => {
     for (let r = 0 ; r < 8; r++) {
         for(let c = 0; c < 8; c++ ) {
 
-            disks[r * 8 + c].style = "";
             disks[r * 8 + c].className = "disk";
 
             if (board[r][c] == black) disks[r * 8 + c].classList.add("black");
             if (board[r][c] == white) disks[r * 8 + c].classList.add("white");
-
-            if (board[r][c] == -1) disks[r * 8 + c].classList.add("gray");
+            if (board[r][c] == gray) disks[r * 8 + c].classList.add("gray");
         }
     }
 }
-
 
 const validMove = (r, c) => {
 
@@ -73,15 +82,14 @@ const validMoves = () => {
         }
     }
 
+    return moves;
+}
+
+const setHints = (moves) => {
     for (let move of moves) {
         board[move[0]][move[1]] = -1
     }
-}
-
-const disableTapZoom = () => {
-    const preventDefault = (e) => e.preventDefault();
-    document.body.addEventListener('touchstart', preventDefault, {passive: false});
-}
+} 
 
 const setBoardSize = () => {
 
@@ -97,7 +105,7 @@ const setBoardSize = () => {
     document.documentElement.style.setProperty('--hole-size', holeSize + 'px');
 }
 
-const clearGray = () => {
+const clearHints = () => {
     for (let r = 0 ; r < 8; r++) {
         for(let c = 0; c < 8; c++ ) {  
             if (board[r][c] == gray) board[r][c] = 0;
@@ -113,8 +121,6 @@ const squareCoords = (touchedSquare) => {
         if (square == touchedSquare) return [Math.floor(i / 8), i % 8];
     }
 }
-
-const reverseColor = () => color = color == black ? white : black;
 
 const getReversedDisks = (r ,c) => {
 
@@ -146,30 +152,121 @@ const getReversedDisks = (r ,c) => {
     return revesedDisks;
 }
 
-const humanTurn = (e) => {    
+let winner = () => {
 
-    let square = e.currentTarget;
-    let [r, c] = squareCoords(square);
+    let whites = 0;
+    let blacks = 0;
 
-    console.log(r, c);
+    for (let r = 0 ; r < 8; r++) {
+        for(let c = 0; c < 8; c++ ) {  
+            if (board[r][c] == white) whites++;
+            if (board[r][c] == black) blacks++;
+        }
+    }
+    return [blacks, whites];
+}
 
-    clearGray();
+let checkWin = () => {
 
-    if (!validMove(r, c)) return;
+    let board1 = board.slice()
 
-    let reversedDisks = getReversedDisks(r, c);
+    console.log(board1);
 
-    console.log(reversedDisks);
+    for (let r = 0 ; r < 8; r++) {
+        for(let c = 0; c < 8; c++ ) {  
+            if (board[r][c] != 1 && board[r][c] != 2) return false;
+        }
+    }
+    return true;
+} 
 
-    reversedDisks.forEach(disk => {
-        board[disk[0]][disk[1]] = color;
-    });
+randomAI = () => {
+
+    let moves = validMoves();
+
+    return moves[Math.floor(Math.random() * moves.length)];
+}
+
+const aiTurn = () => {
+
+    clearHints();
+
+    // let moves = validMoves();
+
+    redrawBoard();
+
+    try {
+
+        let move = randomAI();
+
+        // let move = moves[Math.floor(Math.random() * moves.length)];
+
+        // await new Promise(p => setTimeout(p, 2000));
+
+        let reversedDisks = getReversedDisks(move[0], move[1]);
+
+        reversedDisks.forEach(disk => board[disk[0]][disk[1]] = color);
+
+        draw = false;
+
+    } catch {
+        console.log("PASS AI");
+        if (draw) {console.log(winner()); return}
+        draw = true;
+        alert("PASS AI");
+    };
+
+    if (checkWin()) {console.log(winner()); redrawBoard(); return};
 
     reverseColor();
 
-    validMoves();
+    if (validMoves().length == 0) {
+        console.log("PASS HUMAN");
+        if (draw) {console.log(winner()); return;}
+        draw = true;
+        alert("PASS HUMAN");
+        reverseColor();
+        aiTurn()
+    } else {
+        draw = false;
+        setHints(validMoves());
+        redrawBoard();
+        enableTouch();
+    }
+}
 
-    redrawBoard();
+const humanTurn = (e) => {  
+    
+    let square = e.currentTarget;
+    let [r, c] = squareCoords(square);
+
+    clearHints();
+
+    if (!validMove(r, c)) return;
+
+    disableTouch();
+
+    let reversedDisks = getReversedDisks(r, c);
+
+    reversedDisks.forEach(disk => board[disk[0]][disk[1]] = color);
+
+    reverseColor();
+
+    // validMoves();
+
+    // redrawBoard();
+
+    if (checkWin()) {
+        console.log(winner())
+        redrawBoard();
+    } else {
+        aiTurn();
+    }
+}
+
+const disableTapZoom = () => {
+    const preventDefault = (e) => e.preventDefault();
+    document.body.addEventListener('touchstart', preventDefault, {passive: false});
 }
 
 const enableTouch = () => {
@@ -192,10 +289,9 @@ const disableTouch = () => {
     }
 }
 
-
 const init = () => {
 
-    // disableTapZoom();
+    disableTapZoom();
 
     setBoard();
 
@@ -205,7 +301,7 @@ const init = () => {
     
     showBoard();
 
-    validMoves();
+    setHints(validMoves());
 
     redrawBoard();
 
